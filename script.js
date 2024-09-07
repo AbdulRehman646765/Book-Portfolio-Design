e0a857c84eab096ff6df23668abb1c976fd67767
Abdul Rehman <abdulofficial187@gmail.com>
2024-09-07 15:00:42 +0500

    Complete Portfolio Website

---
+++ script.js
@@ -0,0 +1,111 @@
+const projectBtn = document.getElementById("project-btn");
+const portfolioBox = document.querySelector(".portfolio-box");
+const projectTitle = document.querySelector(".project-title");
+const projectsBox = document.querySelector(".projects-container");
+const backBtn = document.querySelector(".back-btn");
+
+projectBtn.addEventListener("click", () => {
+  portfolioBox.style.display = "none";
+  projectTitle.innerHTML = "My Projects";
+  projectsBox.style.display = "block";
+});
+
+backBtn.addEventListener("click", () => {
+  portfolioBox.style.display = "block";
+  projectTitle.innerHTML = "Latest Project";
+  projectsBox.style.display = "none";
+});
+
+// turn pages when click next or prev button
+const pageTurnBtn = document.querySelectorAll(".next-prev-btn");
+
+pageTurnBtn.forEach((el, index) => {
+  el.onclick = () => {
+    const pageTurnId = el.getAttribute("data-page");
+    const pageTurn = document.getElementById(pageTurnId);
+
+    if (pageTurn.classList.contains("turn")) {
+      pageTurn.classList.remove("turn");
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 - index;
+      }, 500);
+    } else {
+      pageTurn.classList.add("turn");
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 + index;
+      }, 500);
+    }
+  };
+});
+
+// contact me button when click
+const pages = document.querySelectorAll(".book-page.page-right");
+const contactMeBtn = document.querySelector(".btn.contact-me");
+
+contactMeBtn.onclick = () => {
+  pages.forEach((page, index) => {
+    setTimeout(() => {
+      page.classList.add("turn");
+
+      setTimeout(() => {
+        page.style.zIndex = 20 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+//create reverse index function
+let totlePages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+  pageNumber--;
+  if (pageNumber < 0) {
+    pageNumber = totlePages - 1;
+  }
+}
+
+//back profile button when click
+const backProfileBtn = document.querySelector(".back-profile");
+backProfileBtn.onclick = () => {
+  pages.forEach((_, index) => {
+    setTimeout(() => {
+      reverseIndex();
+      pages[pageNumber].classList.remove("turn");
+      setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].style.zIndex = 10 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+//opening animation
+const coverRight = document.querySelector(".cover.cover-right");
+const pageLeft = document.querySelector(".book-page.page-left");
+
+//opening animation (cover right animation)
+setTimeout(() => {
+  coverRight.classList.add("turn");
+}, 2100);
+
+setTimeout(() => {
+  coverRight.style.zIndex = -1;
+}, 2800);
+
+//opening animation (page left or profile page animation)
+setTimeout(() => {
+  pageLeft.style.zIndex = 20;
+}, 3200);
+
+//opening animation (all pages right animation)
+pages.forEach((_, index) => {
+  setTimeout(() => {
+    reverseIndex();
+    pages[pageNumber].classList.remove("turn");
+    setTimeout(() => {
+      reverseIndex();
+      pages[pageNumber].style.zIndex = 10 + index;
+    }, 500);
+  }, (index + 1) * 200 + 2100);
+});
